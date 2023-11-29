d58be62a1379aec3d77f7020c5aab7ab7aaa4f01
David | Wolf Worldwide <88748661+wolfworldwidedigital@users.noreply.github.com>
2023-11-29 17:35:37 +0530

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  /* Vuetify useTheme will not work with SSR enabled, more info - https://vuetify-nuxt-module.netlify.app/guide/server-side-rendering.html#vuetify-themes */
+  // import { useCustomTheme } from '~/composables/custom-theme'
+  // const { isDark } = useCustomTheme()
+
+  /* Update on using the above technique - We now use SSR HTTP Client hints technique instead of the above - https://vuetify-nuxt-module.netlify.app/guide/server-side-rendering.html#ssr-http-client-hints */
+</script>
+
+<template>
+  <div>
+    <!-- <div class="container mx-auto"> -->
+    <v-app
+      class="bg-background text-on-background"
+      style="min-height: 100vh !important"
+    >
+      <!-- <v-app :theme="isDark ? 'dark' : 'light'"> -->
+      <!-- https://vuetifyjs.com/en/features/application-layout/
+        Navbar contains all nav components such as v-app-bar, v-system-bar, v-navigation-drawer, v-bottom-navigation -->
+      <!-- <NavBar /> -->
+      <v-main class="py-4">
+        <slot />
+      </v-main>
+      <!-- https://vuetifyjs.com/en/features/application-layout/
+        TheFooter contains the v-footer -->
+      <!-- <TheFooter /> -->
+    </v-app>
+    <!-- </div> -->
+  </div>
+</template>
+<style></style>
---
+++ my-nuxt-vuetify-layer/components/TheLogo.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  import site from '@/site'
+  const { name, logo } = site
+</script>
+<template>
+  <NuxtLink data-pg-name="Logo" class="flex items-center sm:flex-row" to="/">
+    <v-icon class="text-4xl" :icon="logo"></v-icon>
+    <h5 class="font-extrabold font-serif ml-2 mt-1 text-2xl">{{ name }}</h5>
+  </NuxtLink>
+</template>
+<style scoped>
+  .flex {
+    display: flex;
+  }
+  .items-center {
+    align-items: center;
+  }
+  @media (min-width: 640px) {
+    .sm\:flex-row {
+      flex-direction: row;
+    }
+  }
+  .mt-2 {
+    margin-top: 0.5rem /* 8px */;
+  }
+  .text-primary-600 {
+    --tw-text-opacity: 1;
+    color: rgb(180 100 48 / var(--tw-text-opacity));
+  }
+  :is(.dark .dark\:text-primary-200) {
+    --tw-text-opacity: 1;
+    color: rgb(229 191 170 / var(--tw-text-opacity));
+  }
+  .font-extrabold {
+    font-weight: 800;
+  }
+  .font-serif {
+    font-family: 'Sora', 'Sora fallback', sans-serif;
+  }
+  .ml-2 {
+    margin-left: 0.5rem /* 8px */;
+  }
+  .mt-4 {
+    margin-top: 1rem /* 16px */;
+  }
+  .text-2xl {
+    font-size: 1.5rem /* 24px */;
+    line-height: 2rem /* 32px */;
+  }
+</style>
